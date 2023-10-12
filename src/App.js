b8e04255e9f38e557d5050771b216c153a452438
kimhyesom <kimhyesom1202@gmail.com>
2023-10-12 16:03:49 +0900

    ✨ Setting router of Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
 
 const App = () => {
-  return(
-    <div>
-      App
-    </div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
