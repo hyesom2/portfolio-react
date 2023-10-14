85fcc1ca65d066eba7128e43cfb7509c09878fa0
kimhyesom <kimhyesom1202@gmail.com>
2023-10-15 01:15:51 +0900

    🚧 Setting of components structure

---
+++ src/components/Port.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Port = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Port;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Intro from '../components/Intro';
+import About from '../components/About';
+import Port from '../components/Port';
+import Skill from '../components/Skill';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <>
+      <Header />
+      <Main>
+        <Intro />
+        <About />
+        <Port />
+        <Skill />
+        <Contact />
+        <Footer />
+      </Main>
     </>
   );
 }
