c6f14767d161d1713753225371cca7eb53a6a6e3
kimhyesom <kimhyesom1202@gmail.com>
2023-10-15 15:38:45 +0900

    ✨ Implement Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Main = () => {
+const MainWrapper = styled.div`
+  position: relative;
+  height: 100%;
+  background-color: #eee;
+`;
+const Main = ({ children }) => {
   return (
-    <>
-    </>
+    <MainWrapper id="main" role="main">
+      { children }
+    </MainWrapper>
   );
 }
 
